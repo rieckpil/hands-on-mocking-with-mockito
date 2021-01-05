9e8bdfd3948c0bd534adef8508fc44816730496f
Philip Riecks <mail@philipriecks.de>
2021-01-05 14:34:45 +0100

    non strict Mockito stubbing

--- src/test/java/de/rieckpil/courses/stubbing/EventNotifierTest.java
+++ src/test/java/de/rieckpil/courses/stubbing/EventNotifierTest.java
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class EventNotifierTest {
 
   @Mock
